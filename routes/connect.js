a3a7fc077a2d3403d1414dc478d00e1dbd1f4691
nicolasDelhomme <nicolas.delhomme@slu.se>
2019-10-21 00:32:50 +0300

    Finalised Day1 for the Helsinki course

--- routes/connect.js
+++ routes/connect.js
@@ -4,36 +4,55 @@ var router = express.Router();
 /* for i in {1..64} ; do echo '"'$(printf "User%02d" $i)'":["'$(printf "%02d" $i)'"],'; done */
 userdata =
 {
-    "User01":["01"],
-    "User02":["02"],
-    "User03":["03"],
-    "User04":["04"],
-    "User05":["05"],
-    "User06":["06"],
-    "User07":["07"],
-    "User08":["08"],
-    "User09":["09"],
-    "User10":["10"],
-    "User11":["11"],
-    "User12":["12"],
-    "User13":["13"],
-    "User14":["14"],
-    "User15":["15"],
-    "User16":["16"],
-    "User17":["17"],
-    "User18":["18"],
-    "User19":["19"],
-    "User20":["20"],
+    "ana.montado-rodriguez@helsinki.fi":["01"],
+    "barun.pradhan@helsinki.fi":["02"],
+    "bina.prajapati@helsinki.fi":["03"],
+    "daniel.garton@helsinki.fi":["04"],
+    "denis.belitskin@helsinki.fi":["05"],
+    "dipabarna.bhattacharya@helsinki.fi":["06"],
+    "giorgio.turconi@helsinki.fi":["07"],
+    "heikki.t.virtanen@helsinki.fi":["08"],
+    "honghong.li@helsinki.fi":["09"],
+    "ibrahim.sultan@helsinki.fi":["10"],
+    "ilhan.duru@helsinki.fi":["11"],
+    "jenni.me.virtanen@helsinki.fi":["12"],
+    "johanna.m.anttila@helsinki.fi":["13"],
+    "joni.ollonen@helsinki.fi":["14"],
+    "juliana.ugwu@helsinki.fi":["15"],
+    "jure.zrim@helsinki.fi":["16"],
+    "kai.wang@helsinki.fi":["17"],
+    "kari.salokas@helsinki.fi":["18"],
+    "karina.barreiro@helsinki.fi":["19"],
+    "karri.kaivola@helsinki.fi":["20"],
+    "leevi.lehtonen@helsinki.fi":["21"],
+    "leo.meriranta@helsinki.fi":["22"],
+    "linping.wang@helsinki.fi":["23"],
+    "marion.sinclair-waters@helsinki.fi":["24"],
+    "md.uddin@helsinki.fi":["25"],
+    "mengxia.liu@helsinki.fi":["26"],
+    "meryem.kaya@helsinki.fi":["27"],
+    "mina.kiani@helsinki.fi":["28"],
+    "muhammad.jasim@helsinki.fi":["29"],
+    "neha.rai@helsinki.fi":["30"],
+    "pramin.kaewsatuan@helsinki.fi":["31"],
+    "rigbe.weldatsadik@helsinki.fi":["32"],
+    "samantha.martin@helsinki.fi":["33"],
+    "simon.andersson@helsinki.fi":["34"],
+    "taehee.han@helsinki.fi":["35"],
+    "tanzeela.hanif@helsinki.fi":["36"],
+    "veera.brusila@helsinki.fi":["37"],
+    "ville.j.hietala@helsinki.fi":["38"],
+    "yinyin.wang@helsinki.fi":["39"],
 }
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     for (var property in userdata) {
         if (userdata.hasOwnProperty(property)) {
-            //var baseIP = parseInt(userdata[property]) < 33 ? "86.50.169.222" : "193.167.189.151";
+            var baseIP = parseInt(userdata[property]) < 20 ? "195.148.31.25" : "195.148.31.27";
             //var baseIP = "34.251.25.121"
 	    //var baseIP = "localhost"
-	    var baseIP = "195.148.31.25"
+	    //var baseIP = "195.148.31.25"
             userdata[property].push(baseIP);
         }
     }
