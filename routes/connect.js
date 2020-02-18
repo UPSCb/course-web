034da7b5df66760a8a6d0f75e05e70dc93480c4e
nicolasDelhomme <nicolas.delhomme@slu.se>
2020-02-18 16:54:50 +0100

    Setting up the UPSCb 2020 workshop

--- routes/connect.js
+++ routes/connect.js
@@ -16,6 +16,9 @@ userdata =
 "Group10":["10"],
 "Group11":["11"],
 "Group12":["12"],
+"Group13":["13"],
+"Group14":["14"],
+"Group15":["15"],
 };
 
 /* GET users listing. */
@@ -24,13 +27,13 @@ router.get('/', function(req, res, next) {
         if (userdata.hasOwnProperty(property)) {
             //var baseIP = parseInt(userdata[property]) < 20 ? "195.148.31.25" : "195.148.31.27";
             //var baseIP = "34.251.25.121"
-	    // var baseIP = "localhost"
-	    var baseIP = "44.226.5.112";
+	          var baseIP = "franklin.upsc.se";
+	    // var baseIP = "44.226.5.112";
 	    //var baseIP = "195.148.31.25"
             userdata[property].push(baseIP);
         }
     }
-  res.render('connect', {userdata : userdata, title: 'RSA-NMO-BERLIN-2019' });
+  res.render('connect', {userdata : userdata, title: 'HTSA-UMEA-2020' });
 });
 
 module.exports = router;
--- routes/materials.js
+++ routes/materials.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('materials', { title: 'RSA-NMO-BERLIN-2019' });
+  res.render('materials', { title: 'HTSA-UMEA-2020' });
 });
 
 module.exports = router;
