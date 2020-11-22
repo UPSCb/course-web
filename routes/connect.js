bfb6aed9b9e69b61ef843d632f2bd8344219d140
nicolasDelhomme <nicolas.delhomme@slu.se>
2020-11-22 21:12:32 +0100

    Updated the js files to change the title and add 5 backup instances

--- routes/connect.js
+++ routes/connect.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* for i in {1..64} ; do echo '"'$(printf "User%02d" $i)'":["'$(printf "%02d" $i)'"],'; done */
 userdata =
 {
-"training":["99"],
+"User01":["01"],
+"User02":["02"],
+"User03":["03"],
+"User04":["04"],
+"User05":["05"],
 };
 
 /* GET users listing. */
@@ -13,14 +17,14 @@ router.get('/', function(req, res, next) {
         if (userdata.hasOwnProperty(property)) {
             //var baseIP = parseInt(userdata[property]) < 20 ? "195.148.31.25" : "195.148.31.27";
             //var baseIP = "34.251.25.121"
-	          //var baseIP = "franklin.upsc.se";
-	          var baseIP = "localhost";
+	          var baseIP = "franklin.upsc.se";
+	          //var baseIP = "localhost";
 	          // var baseIP = "44.226.5.112";
 	          //var baseIP = "195.148.31.25"
             userdata[property].push(baseIP);
         }
     }
-  res.render('connect', {userdata : userdata, title: 'RSA-EBI-2020' });
+  res.render('connect', {userdata : userdata, title: 'SLUBI-2020' });
 });
 
 module.exports = router;
