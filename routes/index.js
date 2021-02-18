7dc6b09e28db91ad1e9355cbe56f9ed1c911f312
Nicolas <nicolas.delhomme@umu.se>
2021-02-19 00:27:53 +0100

    TANMO2021 prep

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RSA-NMO-BERLIN-2020' });
+  res.render('index', { title: 'TANMO-CAMBRIDGE-2021' });
 });
 
 module.exports = router;
--- routes/materials.js
+++ routes/materials.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('materials', { title: 'RSA-NMO-BERLIN-2020' });
+  res.render('materials', { title: 'TANMO-CAMBRIDGE-2021' });
 });
 
 module.exports = router;
