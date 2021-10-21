dff98fb318a1e25cbe2939e32226d48df2afba53
Nicolas <nicolas.delhomme@umu.se>
2021-10-21 09:53:56 +0200

    NGG21 website

--- routes/connect.js
+++ routes/connect.js
@@ -4,50 +4,36 @@ var router = express.Router();
 /* for i in {1..64} ; do echo '"'$(printf "User%02d" $i)'":["'$(printf "%02d" $i)'"],'; done */
 userdata =
 {
-"Alessia Vitiello":["01"],
-"Chai Hao Chiu":["02"],
-"Charley Robinson":["03"],
-"Christopher C.H. Mak":["04"],
-"Daniel Plaza":["05"],
-"Dora Luz Cano Ramirez":["06"],
-"Dr Anna Git":["07"],
-"E.L. Bernardo":["08"],
-"Eirlys Tysall":["09"],
-"Elfadil Osman":["10"],
-"Ellen Harrison":["11"],
-"F.N. Khojayori":["12"],
-"Filippo Guerra":["13"],
-"Firas Sadiyah":["14"],
-"Girish Beedessee":["15"],
-"Helena Webb":["16"],
-"Ines Horvat-Menih":["17"],
-"Itsushiro Ko":["18"],
-"Jarka Naser":["19"],
-"Jaruwatana (Sodai) Lotharukpong":["20"],
-"K. Katsuya-Gaviria":["21"],
-"Linda Grillova":["22"],
-"Lucia Arce Cubas":["23"],
-"M.M. Van Kerckvoorde":["24"],
-"Mai-Linh Nu Ton":["25"],
-"Maria Stroyakovski":["26"],
-"Matthew Naish":["27"],
-"Megan Zaman":["28"],
-"Michal Varga":["29"],
-"O. Kranse":["30"],
-"Patrick Colledge":["31"],
-"Pawel Mordaka":["32"],
-"S. Newsad":["33"],
-"Theodoros Danis":["34"],
-"Timing Liu":["35"],
-"Unnati Sonawala":["36"],
-"Wei Jiang":["37"],
-"Wen Cheng":["38"],
-"William Lewis":["39"],
-"Z.X. Low":["40"],
-"Zie Yang":["41"],
-"Bastian Schiffthaler":["42"],
-"Niklas Mähler":["43"],
-"Nicolas Delhomme":["44"],
+"Group01":"[01]",
+"Group02":"[02]",
+"Group03":"[03]",
+"Group04":"[04]",
+"Group05":"[05]",
+"Group06":"[06]",
+"Group07":"[07]",
+"Group08":"[08]",
+"Group09":"[09]",
+"Group10":"[10]",
+"Group11":"[11]",
+"Group12":"[12]",
+"Group13":"[13]",
+"Group14":"[14]",
+"Group15":"[15]",
+"Group16":"[16]",
+"Group17":"[17]",
+"Group18":"[18]",
+"Group19":"[19]",
+"Group20":"[20]",
+"Group21":"[21]",
+"Group22":"[22]",
+"Group23":"[23]",
+"Group24":"[24]",
+"Group25":"[25]",
+"Group26":"[26]",
+"Group27":"[27]",
+"Group28":"[28]",
+"Group29":"[29]",
+"Group30":"[30]"
 };
 
 /* GET users listing. */
@@ -56,14 +42,14 @@ router.get('/', function(req, res, next) {
         if (userdata.hasOwnProperty(property)) {
             //var baseIP = parseInt(userdata[property]) < 20 ? "195.148.31.25" : "195.148.31.27";
             //var baseIP = "34.251.25.121"
-	          var baseIP = "franklin.upsc.se";
+	          //var baseIP = "franklin.upsc.se";
 	          //var baseIP = "localhost";
 	          // var baseIP = "44.226.5.112";
-	          //var baseIP = "195.148.31.25"
+	          var baseIP = "195.148.31.25"
             userdata[property].push(baseIP);
         }
     }
-  res.render('connect', {userdata : userdata, title: 'TANMO-CAMBRIDGE-2021' });
+  res.render('connect', {userdata : userdata, title: 'NGG-HELSINKI-2021' });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TANMO-CAMBRIDGE-2021' });
+  res.render('index', { title: 'NGG-HELSINKI-2021' });
 });
 
 module.exports = router;
